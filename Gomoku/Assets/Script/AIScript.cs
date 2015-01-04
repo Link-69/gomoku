d9c0b8b7863b44aca81da69e57b660c9f467a32d
tiko69 <nicolas.vieira.69360@gmail.com>
2015-01-04 23:37:20 +0100

    final

--- Gomoku/Assets/Script/AIScript.cs
+++ Gomoku/Assets/Script/AIScript.cs
@@ -14,7 +14,6 @@ public class AIScript : MonoBehaviour
     public Vector3 position;
     public int[][] tab;
 
-
     void Start()
     {
         newPion = Instantiate(pionCurseur, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
@@ -25,11 +24,7 @@ public class AIScript : MonoBehaviour
 
     void Update()
     {
-        var get_arbitre = arbitre.gameObject.GetComponent<ArbitreScript>();
-        int i = 0;
-        int j = 0;
-
-        if (Input.GetMouseButtonDown(0))
+       if (Input.GetMouseButtonDown(0))
         {
             if (arbitre.gameObject.GetComponent<ArbitreScript>().CheckPut((int)newPion.transform.position.x, (int)newPion.transform.position.y, id))
             {
@@ -37,14 +32,6 @@ public class AIScript : MonoBehaviour
                 newPion.gameObject.GetComponent<PionScript>().dontTouchId = true;
                 newPion.gameObject.GetComponent<PionScript>().Arbitre = arbitre;
                 IAmaker();
-                
-                //Debug.Log("coucou j = " + j + " i = " + i);
-                //newPion2 = Instantiate(get_arbitre.pion, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
-                //newPion2.gameObject.GetComponent<PionScript>().idPlayer = -1;
-                //newPion2.gameObject.GetComponent<PionScript>().Arbitre = arbitre;
-                //newPion2.gameObject.GetComponent<PionScript>().isPut = true;
-                //tab = arbitre.gameObject.GetComponent<ArbitreScript>().tab;
-                //tab[j][i] = -1;
             }
         }
     }
@@ -65,7 +52,7 @@ public class AIScript : MonoBehaviour
                 if (tab[j][i] == 0)
                 {
                     //check si on peut gg en pos i,j
-                    if (checkwin(tab, i, j) == true)
+                    if (checkwin(i, j) == true)
                     {
                         //poser le pion en i,j
                         if ((get_arbitre.DTrois == true && get_arbitre.DoubleThree(j, i, -1) == false) || get_arbitre.DTrois == false)
@@ -81,7 +68,7 @@ public class AIScript : MonoBehaviour
                     }
 
                     //check pour ne pas perdre en pos i, j
-                    else if (checklose(tab, i, j) == true)
+                    else if (checklose(i, j) == true)
                     {
                         //poser le pion en i,j
                         if ((get_arbitre.DTrois == true && get_arbitre.DoubleThree(j, i, -1) == false) || get_arbitre.DTrois == false)
@@ -97,30 +84,79 @@ public class AIScript : MonoBehaviour
                 }
             }
         }
-        i = -1;
-
-        while (++i < 19)
-        {
-            j = -1;
-            while (++j < 19)
-            {
+		i = -1;
+		
+		while (++i < 19)
+		{
+			j = -1;
+			while (++j < 19)
+			{
 				if (tab[j][i] == 0)
 				{
-                	if ((get_arbitre.DTrois == true && get_arbitre.DoubleThree(j, i, -1) == false) || get_arbitre.DTrois == false)
-                	{
-                    	if (tab[i][j] == 0)
-                    	{
-                      	  newPion2 = Instantiate(get_arbitre.pion, new Vector3(j, i, 0), Quaternion.identity) as GameObject;
-                      	  newPion2.gameObject.GetComponent<PionScript>().idPlayer = -1;
-                      	  newPion2.gameObject.GetComponent<PionScript>().Arbitre = arbitre;
-                      	  newPion2.gameObject.GetComponent<PionScript>().isPut = true;
-                     	   tab[j][i] = -1;
-                     	   return 0;
-                    	}
-                	}
+					if ((get_arbitre.DTrois == true && get_arbitre.DoubleThree(j, i, -1) == false) || get_arbitre.DTrois == false)
+					{
+						if(canEat(i, j) == true)
+						{
+							newPion2 = Instantiate(get_arbitre.pion, new Vector3(j, i, 0), Quaternion.identity) as GameObject;
+							newPion2.gameObject.GetComponent<PionScript>().idPlayer = -1;
+							newPion2.gameObject.GetComponent<PionScript>().Arbitre = arbitre;
+							newPion2.gameObject.GetComponent<PionScript>().isPut = true;
+							tab[j][i] = -1;
+							return 0;
+						}
+					}
 				}
-            }
-        }
+			}
+		}
+
+		i = -1;
+		
+		while (++i < 19)
+		{
+			j = -1;
+			while (++j < 19)
+			{
+				if (tab[j][i] == 0)
+				{
+					if ((get_arbitre.DTrois == true && get_arbitre.DoubleThree(j, i, -1) == false) || get_arbitre.DTrois == false)
+					{
+						if(canEat(i, j) == true)
+						{
+							newPion2 = Instantiate(get_arbitre.pion, new Vector3(j, i, 0), Quaternion.identity) as GameObject;
+							newPion2.gameObject.GetComponent<PionScript>().idPlayer = -1;
+							newPion2.gameObject.GetComponent<PionScript>().Arbitre = arbitre;
+							newPion2.gameObject.GetComponent<PionScript>().isPut = true;
+							tab[j][i] = -1;
+							return 0;
+						}
+					}
+				}
+			}
+		}
+
+		bool put = false;
+
+		while (put == false) 
+		{
+			i = Mathf.FloorToInt(Random.Range(0.0F, 18.0F));
+			j = Mathf.FloorToInt(Random.Range(0.0F, 18.0F));
+			
+			if (tab[j][i] == 0)
+			{
+				if ((get_arbitre.DTrois == true && get_arbitre.DoubleThree(j, i, -1) == false) || get_arbitre.DTrois == false)
+				{
+					if(canFive(i, j) == true)
+					{
+						newPion2 = Instantiate(get_arbitre.pion, new Vector3(j, i, 0), Quaternion.identity) as GameObject;
+						newPion2.gameObject.GetComponent<PionScript>().idPlayer = -1;
+						newPion2.gameObject.GetComponent<PionScript>().Arbitre = arbitre;
+						newPion2.gameObject.GetComponent<PionScript>().isPut = true;
+						tab[j][i] = -1;
+						return 0;
+					}
+				}
+			}
+		}
         return (1);
     }
 
@@ -145,7 +181,7 @@ public class AIScript : MonoBehaviour
         return (true);
     }
 
-    bool checkwin(int[][] tab, int i, int j)
+    bool checkwin(int i, int j)
     {
         var get_arbitre = arbitre.gameObject.GetComponent<ArbitreScript>();
 
@@ -163,7 +199,7 @@ public class AIScript : MonoBehaviour
         return false;
     }
 
-    bool checklose(int[][] tab, int i, int j)
+    bool checklose(int i, int j)
     {
         var get_arbitre = arbitre.gameObject.GetComponent<ArbitreScript>();
 
@@ -190,10 +226,57 @@ public class AIScript : MonoBehaviour
         return false;
     }
 
-    void playbest(int[][] tab)
-    {
-        // je sais pas trop
-    }
+	bool canEat(int i, int j)
+	{
+		var get_arbitre = arbitre.gameObject.GetComponent<ArbitreScript> ();
+		int actuEat = get_arbitre.nbEatPlayer2;
+		
+		get_arbitre.VerifEaten (j, i, -1);
+		if (get_arbitre.nbEatPlayer2 > actuEat)
+			return true;
+		return false;
+	}
+	
+	bool canFive(int i, int j)
+	{
+		if (CanLineFive(j, i, j - 4, i, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j - 4, i + 4, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j, i + 4, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j + 4, i + 4, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j + 4, i, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j + 4, i - 4, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j, i - 4, id, 1) == 5)
+			return true;
+		else if (CanLineFive(j, i, j - 4, i - 4, id, 1) == 5)
+			return true;
+		else
+			return false;
+	}
+	
+	int CanLineFive(int x1, int y1, int x2, int y2, int id, int incr)
+	{
+		if (x2 == x1 && y2 == y1)
+			return (incr);
+		if ((x2 < 0 || x2 > 18) || (y2 < 0 || y2 > 18))
+			return (-1);
+		if (tab[x2][y2] == 1)
+			return (-1);
+		if (x2 < x1)
+			x2 = x2 + 1;
+		else if (x2 > x1)
+			x2 = x2 - 1;
+		if (y2 < y1)
+			y2 = y2 + 1;
+		else if (y2 > y1)
+			y2 = y2 - 1;
+		return (CanLineFive(x1, y1, x2, y2, id, incr + 1));
+	}
 }
 
 
